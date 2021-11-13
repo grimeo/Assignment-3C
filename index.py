b3c7cd6c5d00a3034cd37bce0c1e07e9af8c5006
Romeo <kilazlove@gmail.com>
2021-11-13 21:21:08 +0800

    paste the assignment 2C for editing and applying the function

--- index.py
+++ index.py
@@ -1,2 +1,17 @@
 # Assignment 3C
 # Redo the programs on assignment2 but now with functions
+
+
+money = int(input("Enter the amount of money you have: "))
+apple_Price = int(input("Enter the price of an apple: "))
+
+apples_quantity = 0
+incremented_money = 0
+
+if money < 0 or apple_Price < 0:
+    print("Price or amount is out of bounds.")
+else :
+    while incremented_money <= money - apple_Price:
+        apples_quantity += 1
+        incremented_money += apple_Price
+    print("You can buy " +str(apples_quantity)+ " apples and your change is "+ str(money - incremented_money)+" pesos.")
